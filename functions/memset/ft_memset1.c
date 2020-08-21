03d9ff2efdfd466a23db6f7f195fcc4d5b59b26f
SafaBarka <safa.barka.45876@gmail.com>
2020-08-21 15:57:55 +0100

    about (memset) function + re-code (memset)

--- functions/memset/ft_memset1.c
+++ functions/memset/ft_memset1.c
@@ -22,6 +22,11 @@
 //in general , memeset is a great tool to keep in mind when working with c programs involving memory allocation.
 //remember if you can't initialize it - memset it.
 
+//why not just pass (c) as char ?
+//it's a fair point- if c is only ever converted to an unsigned char before being used in memset, why doesn't the function just receive the parameter directly as char?
+//it turns out that the int parameter type is used for historical reasons.the function memset predates the implementation of functio prototypes in C .without prototypes,parameters cannot be passed as chars,since charcater literals default to ints in c.
+
+//more than for just this historical reason, however,chars are too small to be pushed onto the stack itself, plus using ints eve has performance benefits.
 
 //to use printf
 #include <stdio.h>
@@ -31,27 +36,40 @@
 #include <stdlib.h>
 
 
+void *ft_memset(void *s, int c, size_t n){
+	unsigned int index;
+	printf("address of the variable pointed by void pointer (s)= %p\n",s);
+	unsigned char *memory = s, value = c;
+	printf("address of the variable pointed by unsigned char pointer (memory) = %p\n",memory);
+	for(index = 0; index < n; index++){
+		memory[index] = value;
+	}
+	return memory;
+}
+
 int main(){
 
 	char memory[8];
-
 	//fill the memory block with null bytes
 	
-	memset(memory,'\0',8);
-	printf("memory = %s(end)\n",memory);
+	ft_memset(memory,'\0',8);
+
+	printf("memory  = %s(end)\n",memory);
 
 	printf("-------------------------\n");
 
 	//fill the memory block with hashes
 	
-	memset(memory,'#',8);
-	printf("memory = %s\n",memory);
+	ft_memset(memory,'#',8);
+
+	printf("memory  = %s\n",memory);
 
 	printf("--------------------------\n");
 
 	//fill the memory block with dollar sign
 	
-	memset(memory,'$',8);
+	ft_memset(memory,'$',8);
+
 	printf("memory = %s\n",memory);
 
 	printf("--------------------------\n");
@@ -63,5 +81,14 @@ int main(){
 	printf("sizeof char %lu bytes\n",sizeof(char));
 	printf("size of 'a' %lu bytes\n",sizeof('a'));
 
+	printf("------------------------------------\n");
+
+	ft_memset(memory +1 ,'.',3);//$...$$$$
+	printf("memory = %s\n",memory);
+
+	printf("------------------------------------\n");
+
+	ft_memset(memory,-1,8);//-1 -1 -1 -1 -1 -1 -1
+	printf("memory = %s\n",memory);
 	return (EXIT_SUCCESS);
 }
