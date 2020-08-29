bc8279c62ea8a62935a5ea8952a86464fb63864a
SafaBarka <safa.barka.45876@gmail.com>
2020-08-29 10:19:31 +0100

    re-code the (memmove) function + call it (ft_memmove) + test it

--- functions/memmove/ft_memmove.c
+++ functions/memmove/ft_memmove.c
@@ -18,91 +18,94 @@
 
 #include <stdio.h>
 
-void *ft_memcpy(void *dst , const void *src , size_t n){
-	size_t i =0;
-	while (i < n){
-		*((unsigned char *)dst + i) = *((unsigned char *)src + i);
-		i++;
+
+void *ft_memmove(void *dst, const void *src, size_t n){
+
+	size_t i = 0;
+	if (dst == src)
+		return dst;
+	if (dst < src)
+		while ( i < n ){
+			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+			i++;
 	}
+	else
+		while (n--)
+			*((unsigned char *)dst + n) = *((unsigned char *)src +n);
 	return dst;
-}
-
-void *ft_memcpy1(void *dst, const void *src, size_t n){
-
-	int  i =(int)n;
-	
-	while (i--)
-		*((unsigned char *)dst + i) = *((unsigned char *)src +i);
-	return dst;
-	
+		
 }
 int main(){
-	//overlap case :dst after src
 
 	printf("1---------------------------------------\n");
 
-	char str[]="THis is an example";
-
-	printf("memcpy return value : %s\n",(char *)ft_memcpy(str + 7,str,10));//THis isTHie
-
-	printf("dst = %s\n",(char *)(str+7));//This isTHie
-	
-	printf("src = %s\n",(char *)(str));//THis isTHie isTHie
-
-
-	printf("2---------------------------------------\n");
-
 	char str1[]="THis is an example";
 
-	printf("memcpy return value : %s\n",(char *)ft_memcpy1(str1 + 7,str1,10));//THis is ane
+	printf("ft_memmove return value : %s\n",(char *)ft_memmove(str1 + 7,str1,10));
 
-	printf("dst = %s\n",(char *)(str1+7));//This is ane
+	printf("dst = %s\n",(char *)(str1+7));
 	
-	printf("src = %s\n",(char *)(str1));//THis isTHis is ane
+	printf("src = %s\n",(char *)(str1));
 
 
-	printf("3---------------------------------------\n");
 
 	char str2[]="THis is an example";
 
-	printf("memcpy return value : %s\n",(char *)memcpy(str2 + 7,str2,10));//THis is anle
+	printf("memmove return value : %s\n",(char *)memmove(str2 + 7,str2,10));
 
-	printf("dst = %s\n",(char *)(str2+7));//This is anle
+	printf("dst = %s\n",(char *)(str2+7));
+
+	printf("src = %s\n",(char *)(str2));
+
+
+	printf("2---------------------------------------------\n");
 	
-	printf("src = %s\n",(char *)(str2));//THis isTHis is ane
+	char str3[]="THi is an example";
 
+	printf("ft_memmove return value : %s\n",(char *)ft_memmove(str3 ,str3 + 3,10));
 
-	printf("4-------------------------------------------\n");
-
-	char str3[]="THis is an example";
-
-	printf("memmove return value : %s\n",(char *)memmove(str3 + 7,str3,10));//THis is ane
-
-	printf("dst = %s\n",(char *)(str3+7));//THis is ane
-
-
-	printf("src = %s\n",(char *)(str3));//THis isTHis is ane
-
-	printf("****************************************************************\n");
-	//overlap case : src after dst 
-	printf("5---------------------------------------------\n");
+	printf("dst = %s\n",(char *)(str3));
 	
-	char str5[]="THis is an example";
+	printf("src = %s\n",(char *)(str3 + 3));
 
-	printf("memcpy return value : %s\n",(char *)memcpy(str5,str5 + 3,10));
-
-	printf("dst = %s\n",(char *)(str5));
 	
-	printf("src = %s\n",(char *)(str5 + 7));
+	char str4[]="THi is an example";
 
-	printf("6---------------------------------------------\n");
+	printf("memmove return value : %s\n",(char *)memmove(str4 ,str4 + 3,10));
+
+	printf("dst = %s\n",(char *)(str4));
 	
-	char str6[]="THis is an example";
+	printf("src = %s\n",(char *)(str4 + 3));
 
-	printf("memmove return value : %s\n",(char *)memmove(str6 ,str6 + 3,10));
+	printf("8---------------------------------------------\n");
 
-	printf("dst = %s\n",(char *)(str6));
+	char str5[]="hello world!";
 	
-	printf("src = %s\n",(char *)(str6 + 7));
+	printf("ft_memmove return value : %s\n",(char *)ft_memmove(str5 + 3,str5,7));
+
+	printf("dst = %s\n",(char *)(str5+6));
+	
+	printf("src = %s\n",(char *)(str5));
+
+	char str6[]="hello world!";
+	
+	printf("memmove return value : %s\n",(char *)memmove(str6 + 3,str6,7));
+
+	printf("dst = %s\n",(char *)(str6+6));
+	
+	printf("src = %s\n",(char *)(str6));
+
+	printf("9------------------------------------------------\n");
+
+	char s1[]="barka";
+
+	char *s2="safa";
+	
+	printf("ft_memmove return value : %s\n",(char *)ft_memmove(s1 ,s2,7));
+
+	printf("dst = %s\n",(char *)(s1));
+	
+	printf("src = %s\n",(char *)(s2));
+
 
 }
