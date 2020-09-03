c9802b0a583823ac3ac94e715b635b41b90d817f
SafaBarka <safa.barka.45876@gmail.com>
2020-09-03 23:41:20 +0100

    about ASCII

--- functions/isascii/ft_isascii.c
+++ functions/isascii/ft_isascii.c
@@ -4,6 +4,13 @@
 //The isascii function is defined on all integer values.
 //return value : The isascii() function shall return non-zero if c is a 7-bit US-ASCII character set ; otherwise, it shall return 0.
 
+//ASCII is an acronym for American Standard Code for Information Interchange .
+//it is a code that uses numbers to represent characters .Each letter is assigned a number between 0 ans 127 . A upper and lower case character are assigned different numbers.for example the character A is assigned the decimal number 65, while a is assigned decimal 97 .
+//when is Ascii code used?
+//when a computer sends data the keys you press or the text you send and receive is sent as a bunch of numbers.These numbers represent the characters you typed or generated.Because the range of standard ASCII is 0 to 127 it only requires 7 bits or 1 byte of data. for example to send the string cactus.io as ascii it would translate to 99 97 99 116 117 115 46 105 111.Microprocessors only understand bits and bytes.To it everrything is a sequence of bitsStandard Code for Information Interchange .
+//it is a code that uses numbers to represent characters .Each letter is assigned a number between 0 ans 127 . A upper and lower case character are assigned different numbers.for example the character A is assigned the decimal number 65, while a is assigned decimal 97 .
+//when is Ascii code used?
+//when a computer sends data the keys you press or the text you send and receive is sent as a bunch of numbers.These numbers represent the characters you typed or generated.Because the range of standard ASCII is 0 to 127 it only requires 7 bits or 1 byte of data. for example to send the string cactus.io as ascii it would translate to 99 97 99 116 117 115 46 105 111.Microprocessors only understand bits and bytes.To it everrything is a sequence of bits.  
 #include <ctype.h>
 #include <stdio.h>
 
