c5a3270be456b50f1fb6cda36ed6e1611e610c15
SafaBarka <safa.barka.45876@gmail.com>
2020-09-06 18:45:31 +0100

    re-code (ft_memcpy) function + examples

--- functions/memcpy/ft_memcpy1.c
+++ functions/memcpy/ft_memcpy1.c
@@ -4,13 +4,8 @@
 
 void *ft_memcpy(void *dst, void *src,size_t n){
 
-        int i = -1;
-        int j = n;
-        if (dst == NULL && src == NULL && n== 0)
-                return dst;
-
-        while(++i < j )
-                *((char *)dst + i)=*((char *)src + i);
+        while(n--)
+                *((char *)dst + n)=*((char *)src + n);
         return dst;
 }
 
@@ -24,13 +19,7 @@ int main(){
 	//segmentation fault 
 	
 	/*
-	printf("return value = %s\n",(char *)memcpy(dst,src,3));
-	printf("src = %s\n",src);
-	printf("dst = %s\n",dst);
-
-	*/
-
-	/*
+	//segfault because of the instruction in the loop.
 	printf("return value = %s\n",(char *)ft_memcpy(dst,src,3));
 	printf("src = %s\n",src);
 	printf("dst = %s\n",dst);
@@ -39,38 +28,34 @@ int main(){
 
 	printf("2-----------------------------------\n\n");
 
-	//set the size to 0 ==> no segmentation fault
-
-	printf("return value = %s\n",(char *)memcpy(dst,src,0));//null
-	printf("src = %s\n",src);//null
-	printf("dst = %s\n",dst);//null
-
-	printf("\n");
+	//because n = 0 , it will not enter the loop and return null.
 
 	printf("return value = %s\n",(char *)ft_memcpy(dst,src,0));//null
 	printf("src = %s\n",src);//null
 	printf("dst = %s\n",dst);//null
 
-	printf("\n");
 
 	printf("3---------------------------------\n\n");
 	
 	printf("segmentation fault case\n");
 	//segmantation fault
-	//if src or dst is null , and the size != 0 
+
 	char *src1 = NULL;
 	char dst1[] = "hello";
-/*
+	/*
+ 	//segfault because of the instruction in the loop. (accessing an address using a NULL pointer).
 	printf("return value = %s\n",(char *)memcpy(dst1,src1,2));
 	printf("src1 = %s\n",src1);
 	printf("dst1 = %s\n",dst1);
-*/	
-	
-/*	printf("return value = %s\n",(char *)ft_memcpy(dst1,src1,2));
-	printf("src1 = %s\n",src1);
-	printf("dst1 = %s\n",dst1);
-*/	
+	*/	
 
 	printf("4-------------------------------------\n\n");
 
+	char *src2 = "h";
+
+	char dst2[]="yes";
+	
+	printf("return value = %s\n",(char *)ft_memcpy(dst2,src2,5));//"h"
+	printf("src1 = %s\n",src2);//"h"
+	printf("dst1 = %s\n",dst2);//"h"
 }
