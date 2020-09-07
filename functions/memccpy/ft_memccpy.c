3f212ecc18a6f9332c6309536914cc2233af0d7b
SafaBarka <safa.barka.45876@gmail.com>
2020-09-07 16:10:28 +0100

    more about the predefined function in c (memcpy)

--- functions/memccpy/ft_memccpy.c
+++ functions/memccpy/ft_memccpy.c
@@ -1,19 +1,19 @@
 //void  *memccpy(void *dest, const void *src, int c, size_t n);
-
 //la fonction memccpy() copie au plus n octets de la zone memoire src vers la zone memoire dest.s'arretant des qu'elle rencontre le caractere c.
-
 //la fonction memccpy() renvoie un pointeur sur le caractere immediatement apres c dans la zone dest, ou NULL si c'est n'a pas ete trouve dans les n premiers carateres de src.
 
 //memccpy - copy memory area
-
 //include <string.h>
-
 //copies no more than n bytes from memory area src to memory area dest, stopping when the charactere  found.
-
 //if memory areas overlap the results are undefined.
-
 // the memccpy() function return a  pointer to the next character in the dest after c, or NULL if c was not found in the first n characters of src.
 
+//the memccpy() function shall copy bytes from memory area src into dest, stopping after the first occurence of byte c(converted to unsigned char) is copied, or after n bytes are copied.whichever comes first. if copying takes place between objects that overlap, the behavior is undefined.//the memccpy() function shall return a pointer to the byte after the copy of c in src. or a null pointer if c was found in the first n bytes of src.
+//the memccpy() function does not check for the overflow of the receiving memory area.
+//dest: points to the memory area to which memccpy() copies bytes.
+//src: points to the memory area from which memccpy() copies bytes,
+//c :represent the unsigned char that memccpy() copied. after the first occurence of this parameter, memccpy() stops its copy operation unless n bytes were copied first.
+//n: represents the number of bytes that memccpy() copied. This operation stops after n bytes has been reached, unless c came first.
 
 #include <string.h>
 
@@ -21,9 +21,9 @@
 
 void *ft_memccpy(void *dst, const void *src, int c, size_t n){
 
-	int i = 0;
+	size_t i = 0;
 
-	while (i < n)                                                                           {
+	while (i < n)                                                              {
 		*((unsigned char *)dst + i) =*((unsigned char *)src + i);
 		i++;
 		if ( ((unsigned char *)src)[i-1] == (unsigned char)c)
@@ -38,9 +38,6 @@ int main(){
 
 	char *src = NULL;
 
-	printf("return value : %s\n",(char *)memccpy(dst, src ,'c', 0));//null
-        printf("dst = %s\n",dst);//null
-
 	printf("return value : %s\n",(char *)ft_memccpy(dst, src ,'c', 0));//null
         printf("dst = %s\n",dst);//null
 
@@ -50,11 +47,7 @@ int main(){
 	//segfault case
 	printf("segmentation fault case :\n");
 	/*
-	printf("return value : %s\n",(char *)memccpy(dst, src ,'c', 2));
-        printf("dst = %s\n",dst);
-	*/
-
-        /*printf("return value : %s\n",(char *)ft_memccpy(dst, src ,'c', 2));
+	printf("return value : %s\n",(char *)ft_memccpy(dst, src ,'c', 2));
         printf("dst = %s\n",dst);
 	*/
 
@@ -63,9 +56,6 @@ int main(){
 	char dst1[]="hello";
 
 	char *src1 = NULL;
-
-        printf("return value : %s\n",(char *)memccpy(dst1, src1 ,'c', 0));//null
-        printf("dst = %s\n",dst1);//hello
 
         printf("return value : %s\n",(char *)ft_memccpy(dst1, src1 ,'c', 0));//null
         printf("dst = %s\n",dst1);//hello
