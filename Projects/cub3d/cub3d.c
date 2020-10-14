fe67e1e63cf21fd619cf4531d38245756ece22e2
safa-barka <safa-barka>
2020-10-14 14:50:07 +0100

    coding put_pixel function (for coloring my pixels ) + pushing the image to window

--- Projects/cub3d/cub3d.c
+++ Projects/cub3d/cub3d.c
@@ -28,7 +28,21 @@ void *mlx_win;
 t_data  my_image;
 
 //-------------------------------
+    //fill the image using formula (X position * 4 + 4 * Line_size * Y position)
 
+void put_pixel(int x, int y, int color)
+{
+    char *dst;
+    //y * my_image.line_length *4 + x * 4
+    //4(y * my_image.line_length + x)
+    //(32/8)*(y * my_image.line_length + x)
+    //(my_image.bits_per_pixel/8) * (y * my_image.line_length + x)
+    dst = my_image.addr + ((my_image.bpp/8) * (y * my_image.line_length + x));
+
+    //color information is stored as four individual 8-bit unsigned integer components.
+    *(unsigned int*)dst = color;
+}
+//-------------------------------
 void initialize_mlx()
 {
     //establish a connection to the correct graphical system
@@ -46,6 +60,20 @@ void initialize_mlx()
     //the current data address
     my_image.addr = mlx_get_data_addr(my_image.img, &my_image.bpp, &my_image.line_length,&my_image.endian);
     
+    //fill the image using formula (X position * 4 + 4 * Line_size * Y position)
+    put_pixel(0,0,0xEF4135);
+    put_pixel(1,0,0xEF4135);
+    put_pixel(2,0,0xEF4135);
+    put_pixel(3,0,0xEF4135);
+
+ 
+    //Display the image
+    //use mlx_put_image_to_window() function.
+    //pushing images to a window.
+    //first 0 is the x position of your image on the window.
+    //the second 0 is the y position.
+    mlx_put_image_to_window(mlx,mlx_win,my_image.img,0,0);
+
     //initiate the window rendering 
     mlx_loop(mlx);
 
