9c8c243ab20e27d54a77ace28de459ad26936a77
safa-barka <safa-barka>
2020-10-15 10:43:26 +0100

    fix put_pixel() function -> correct the math formula of coloring a pixel + coloring the window

--- Projects/cub3d/cub3d.c
+++ Projects/cub3d/cub3d.c
@@ -30,17 +30,16 @@ t_data  my_image;
 //-------------------------------
     //fill the image using formula (X position * 4 + 4 * Line_size * Y position)
 
-void put_pixel(int x, int y, int color)
+void put_pixel(int x, int y, long  color)
 {
     char *dst;
-    //y * my_image.line_length *4 + x * 4
-    //4(y * my_image.line_length + x)
-    //(32/8)*(y * my_image.line_length + x)
-    //(my_image.bits_per_pixel/8) * (y * my_image.line_length + x)
-    dst = my_image.addr + ((my_image.bpp/8) * (y * my_image.line_length + x));
+    //y * my_image.line_length  + x * 4
+    //y * my_image.line_length + x * (32/8)
+    //y * my_image.line_length + x * (my_image.bits_per_pixel/8) 
+    dst = my_image.addr + (y * my_image.line_length + x * (my_image.bpp/ 8));
 
     //color information is stored as four individual 8-bit unsigned integer components.
-    *(unsigned int*)dst = color;
+    *(unsigned long int *)dst = color;
 }
 //-------------------------------
 void initialize_mlx()
@@ -51,22 +50,27 @@ void initialize_mlx()
     //create a window
     //width = 1920
     //height = 1080
-    mlx_win = mlx_new_window(mlx, 1920, 1080,"cub3d");
+    mlx_win = mlx_new_window(mlx, 1200, 1000,"cub3d");
 
     //create an image
-    my_image.img = mlx_new_image(mlx,1920,1080);
+    my_image.img = mlx_new_image(mlx,1200,1000); //1920 - 1080
 
     //the image address
     //the current data address
     my_image.addr = mlx_get_data_addr(my_image.img, &my_image.bpp, &my_image.line_length,&my_image.endian);
-    
-    //fill the image using formula (X position * 4 + 4 * Line_size * Y position)
-    put_pixel(0,0,0xEF4135);
-    put_pixel(1,0,0xEF4135);
-    put_pixel(2,0,0xEF4135);
-    put_pixel(3,0,0xEF4135);
+       
+   for(int y = 0 ; y <1000; y++)
+   {
+            
+            for (int x = 0 ; x <1200; x++)
+            {
+              printf("(%d, %d)  ",x,y);
+              put_pixel(x,y,0xEF4135);
+            }
+            printf("\n");
+           
+   }
 
- 
     //Display the image
     //use mlx_put_image_to_window() function.
     //pushing images to a window.
