1b0444d18588b232d609ba14d912e2a83d3ee469
sbarka-safa <sbarka@e1r12p8.1337.ma>
2021-01-02 12:15:11 +0100

    fix bitmap

--- ft_save.c
+++ ft_save.c
@@ -12,25 +12,14 @@
 
 #include "cub3d.h"
 
-int		*get_colors(int color)
-{
-	int		*colors;
 
-	if (!(colors = malloc(3 * sizeof(int))))
-		ft_err("problem memory");
-	colors[0] = ((color >> 16) & 0xFF);
-	colors[1] = ((color >> 8) & 0xFF);
-	colors[2] = ((color) & 0xFF);
-	return (colors);
-}
 
 void	ft_initialize_save(void)
 {
 	g_c.bitm.w = g_c.w;
 	g_c.bitm.he = g_c.h;
-	g_c.bitm.bc = 24;
-	g_c.bitm.wb = ((g_c.bitm.w * g_c.bitm.bc + 31) / 32) * 4;
-	g_c.bitm.imgs = g_c.bitm.wb * g_c.bitm.he;
+	g_c.bitm.bc = 32;
+	g_c.bitm.imgs = g_c.bitm.w * g_c.bitm.he *4;
 	g_c.bitm.bs = 40;
 	g_c.bitm.bob = 54;
 	g_c.bitm.fs = 54 + g_c.bitm.imgs;
@@ -45,25 +34,23 @@ int		ft_save(void)
 {
 	ft_initialize_save();
 	ft_header();
-	while (g_c.bitm.ro > 0)
+	int *pixel;
+	int pos;
+	g_c.bitm.fd = open("cub3d.bmp", O_WRONLY | O_CREAT, 0644);
+	write(g_c.bitm.fd, g_c.bitm.h, 54);
+	while (g_c.bitm.ro > -1)
 	{
 		while (g_c.bitm.co < g_c.w)
 		{
-			g_c.bitm.color = get_colors(g_c.img.addr[(g_c.bitm.he -
-			g_c.bitm.ro) * g_c.bitm.w + g_c.bitm.co]);
-			g_c.bitm.buf[g_c.bitm.ro * g_c.bitm.wb + g_c.bitm.co * 3 + 0] =
-			g_c.bitm.color[2];
-			g_c.bitm.buf[g_c.bitm.ro * g_c.bitm.wb + g_c.bitm.co * 3 + 1] =
-			g_c.bitm.color[1];
-			g_c.bitm.buf[g_c.bitm.ro * g_c.bitm.wb + g_c.bitm.co * 3 + 2] =
-			g_c.bitm.color[0];
-			free(g_c.bitm.color);
+			pos = g_c.bitm.ro * g_c.bitm.w + g_c.bitm.co;
+			pixel = (int *)(g_c.img.addr) + pos;
+			write(g_c.bitm.fd, pixel, 4);
 			g_c.bitm.co++;
 		}
 		g_c.bitm.ro--;
 		g_c.bitm.co = 0;
 	}
-	ft_save_file();
+	close(g_c.bitm.fd);
 	exit(EXIT_SUCCESS);
 	return (0);
 }
@@ -71,8 +58,6 @@ int		ft_save(void)
 void	ft_save_file(void)
 {
 	g_c.bitm.fd = open("cub3d.bmp", O_WRONLY | O_CREAT, 0644);
-	write(g_c.bitm.fd, g_c.bitm.h, 54);
-	write(g_c.bitm.fd, g_c.bitm.buf, g_c.bitm.imgs);
 	close(g_c.bitm.fd);
 	free(g_c.bitm.buf);
 }
